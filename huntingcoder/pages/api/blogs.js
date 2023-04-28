77cff63a434ee9c842f24ae4b56ca9410ec254c2
Darsh Kejriwal <darsh.kejriwal@gmail.com>
2023-04-29 01:37:50 +0530

    h

--- huntingcoder/pages/api/blogs.js
+++ huntingcoder/pages/api/blogs.js
@@ -2,12 +2,16 @@
 import * as fs from 'fs';
 
 export default async function handler(req, res) {
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of 8f9a2d0 (Tutorial 29)
     let data = await fs.promises.readdir("blogdata");
-    data = data.slice(0, parseInt(req.query.count)) 
     let myfile; 
     let allBlogs = [];
       for (let index = 0; index < data.length; index++) {
-        const item = data[index]; 
+        const item = data[index];
+          console.log(item)
           myfile = await fs.promises.readFile(('blogdata/' + item), 'utf-8') 
           allBlogs.push(JSON.parse(myfile))
       }
