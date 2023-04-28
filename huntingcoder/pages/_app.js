2f81d0eca8033f63a623be427a832d61fa1d1f68
Darsh Kejriwal <darsh.kejriwal@gmail.com>
2023-04-29 01:40:33 +0530

    Revert "Tutorial 14"
    This reverts commit 231d373aff458526dd0dca0f391acfaea8ea54bb.

--- huntingcoder/pages/_app.js
+++ huntingcoder/pages/_app.js
@@ -1,11 +1,7 @@
 import '../styles/globals.css'
-import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
-  return <>
-  <Navbar/>
-  <Component {...pageProps} />
-  </>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
