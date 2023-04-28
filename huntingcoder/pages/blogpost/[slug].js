5de81c4077bd167bed4f76e2b9b452f6b6e3fb6f
Darsh Kejriwal <darsh.kejriwal@gmail.com>
2023-04-29 01:39:21 +0530

    Revert "Tutorial 20"
    This reverts commit b578836b8af177056a6c6e3336d13705036f27ff.

--- huntingcoder/pages/blogpost/[slug].js
+++ huntingcoder/pages/blogpost/[slug].js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { useRouter } from 'next/router'
 import styles from '../../styles/BlogPost.module.css' 
 
@@ -71,27 +71,16 @@ export async function getStaticProps(context) {
 }
 =======
 const slug = () => {
-    const [blog, setBlog] = useState();
     const router = useRouter();
-    useEffect(() => {
-        if (!router.isReady) return;
-        const { slug } = router.query;
-        fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
-            return a.json();
-          })
-            .then((parsed) => {
-              setBlog(parsed)
-            })
-    }, [router.isReady])
-    
-    
+    const { slug } = router.query;
     return <div className={styles.container}>
         <main className={styles.main}>
-        <h1>{blog && blog.title}</h1>
+        <h1>Title of the page {slug}</h1>
         <hr />
         <div>
-            {blog && blog.content}
-            </div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem nulla repudiandae sint facilis, sunt corrupti numquam id illo. Ut deserunt animi iste voluptatum!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus maxime rem earum repudiandae, cum possimus quae assumenda nulla culpa. Odit architecto repellendus non, unde recusandae placeat nisi perferendis quod nesciunt! Dolorum sapiente et sint consequuntur earum blanditiis iusto reprehenderit molestiae quia eligendi? Exercitationem, officia nobis!
+        </div>
         </main>
     </div>;
 };
